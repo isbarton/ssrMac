5ae97f5afd0c3e14d8a545fba3ee6fad66125723
ssrlive <ssrlivebox@gmail.com>
2018-02-01 17:49:25 +0800

    copy QR code to PasteBoard

--- ssrMac/SWBQRCodeWindowController.m
+++ ssrMac/SWBQRCodeWindowController.m
@@ -28,6 +28,51 @@
     }
 }
 
++ (NSImage *) generateImageFromWebView:(WebView *)webView size:(NSSize)size {
+    NSView *webFrameViewDocView = [[[webView mainFrame] frameView] documentView];
+    NSRect cacheRect = [webFrameViewDocView bounds];
+    
+    NSBitmapImageRep *bitmapRep = [webFrameViewDocView bitmapImageRepForCachingDisplayInRect:cacheRect];
+    [webFrameViewDocView cacheDisplayInRect:cacheRect toBitmapImageRep:bitmapRep];
+    
+    NSSize imgSize = cacheRect.size;
+    if (imgSize.height > imgSize.width) {
+        imgSize.height = imgSize.width;
+    }
+    
+    NSRect srcRect = NSZeroRect;
+    srcRect.size = imgSize;
+    srcRect.origin.y = cacheRect.size.height - imgSize.height;
+    
+    NSRect destRect = NSZeroRect;
+    destRect.size = imgSize;
+    
+    NSImage *webImage = [[NSImage alloc] initWithSize:imgSize];
+    [webImage lockFocus];
+    [bitmapRep drawInRect:destRect
+                 fromRect:srcRect
+                operation:NSCompositeCopy
+                 fraction:1.0
+           respectFlipped:YES
+                    hints:nil];
+    [webImage unlockFocus];
+    
+    NSSize defaultDisplaySize;
+    defaultDisplaySize.height = size.height * (imgSize.height / imgSize.width);
+    defaultDisplaySize.width = size.width;
+    [webImage setSize:defaultDisplaySize];
+    
+    return webImage;
+}
+
+- (IBAction)copyToPasteboardClicked:(NSButton *)sender {
+    // Then copy _qrCode and image to pasteboard
+    NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
+    [pasteboard clearContents];
+    NSImage *image = [[self class] generateImageFromWebView:_webView size:NSMakeSize(256., 256.)];
+    [pasteboard writeObjects:@[_qrCode, image]];
+}
+
 -(void)dealloc {
     self.webView.frameLoadDelegate = nil;
 }
